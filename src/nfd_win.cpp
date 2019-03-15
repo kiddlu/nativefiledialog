e4208a6e204499463e552d26b43e3a9745e43c80
kiddlu <kidd.dawny.lu@gmail.com>
2019-03-15 16:45:02 +0800

    fix warming
    Signed-off-by: kiddlu <kidd.dawny.lu@gmail.com>

--- src/nfd_win.cpp
+++ src/nfd_win.cpp
@@ -16,6 +16,11 @@
 #ifdef __MINGW32__
 // Explicitly setting NTDDI version, this is necessary for the MinGW compiler
 #define NTDDI_VERSION NTDDI_VISTA
+
+#ifdef _WIN32_WINNT
+#undef _WIN32_WINNT
+#endif
+
 #define _WIN32_WINNT _WIN32_WINNT_VISTA
 #endif
 
